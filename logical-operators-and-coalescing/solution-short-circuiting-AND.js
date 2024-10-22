2d9751985a8bd2c895beea900b6700f47de18fe6
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2024-10-23 00:11:05 +0530

    AND challenge added

---
+++ logical-operators-and-coalescing/solution-short-circuiting-AND.js
@@ -0,0 +1,11 @@
+const accountBalanceUsd = '$45,000,000,000 🤑💰';
+const swissBankPassCodesArr = [1234,5678,9876,3434];
+
+function authenticationCheck(passCode){
+/*
+Solution:
+*/
+   swissBankPassCodesArr.includes(passCode) && console.log(accountBalanceUsd);
+}
+
+authenticationCheck(3434);
